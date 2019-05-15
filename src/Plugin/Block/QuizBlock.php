716fd5e08febd0fc2d94f28f8063d706aef91b59
leofishman <leofishman@gmail.com>
2019-05-14 22:19:17 -0300

    Configurable quiz block showing quiz title

---
+++ src/Plugin/Block/QuizBlock.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Drupal\leo_quiz\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides a 'QuizBlock' block.
+ *
+ * @Block(
+ *  id = "quiz_block",
+ *  admin_label = @Translation("Quiz block"),
+ * )
+ */
+class QuizBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [
+          ] + parent::defaultConfiguration();
+  }
+
+
+  
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $query = \Drupal::entityQuery('node');
+    $query->condition('status', 1);
+    $query->condition('type', 'leo_quiz')
+    ->addTag('distinct');
+    $nids = $query->execute();
+    $nodes = node_load_multiple($nids);
+    foreach ($nodes as $nid => $node){
+      $options[$nid] = $node->getTitle();
+    }
+    $form['quiz_to_show'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Quiz to Show'),
+      '#options' => $options,
+      '#description' => $this->t('Select the Quiz to show in the block'),
+      '#default_value' => $this->configuration['quiz_to_show'],
+      '#weight' => '0',
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $this->configuration['quiz_to_show'] = $form_state->getValue('quiz_to_show');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    //TODO atach javascript behavior to get the quiz api and draw the questions and submit button
+    $build = [];
+    $build['quiz_block_quiz_to_show']['#markup'] = '<p>' . \Drupal::entityTypeManager()->getStorage('node')->load($this->configuration['quiz_to_show'])->getTitle() . '</p>';
+
+    return $build;
+  }
+
+}

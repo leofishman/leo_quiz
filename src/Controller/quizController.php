e126de7afc2ca1949008075051f5748ff37cd625
leofishman <leofishman@gmail.com>
2019-05-14 21:04:20 -0300

    better json response and only the last question

--- src/Controller/quizController.php
+++ src/Controller/quizController.php
@@ -56,39 +56,41 @@ class quizController extends ControllerBase {
     $node = Node::load($nid);
     $title = $node->title->value;
     if ($title){
-
-    
+      $tempstore = \Drupal::service('tempstore.private')->get('leo_quiz');
+      $tempstore->get('leo_quiz_session', $quiz_status);
+      $last_question = is_null($quiz_status[$nid]['question'])?0:$quiz_status[$nid]['question'];
       $type = $node->getType();
       $status = $node->status->value;
-      $response_array = ['Title', $node->getTitle()];
-      $answers = [];
-
+      $response_array['Title'] = $node->getTitle();
       if (($type == 'leo_quiz') && ($status == 1)) {
-        foreach ($node->get('field_quiz') as $para) {
-          if ($para->entity->getType() == 'leo_quiz') {   // e.g., "main_content" or "social_media"
-            $quiz = $para->entity;   
-            $question = ['question',$quiz->field_question->value];
-            $answer = $quiz->field_answer->value;
-            array_push($answers, $answer);
-            $options = $quiz->field_option->getValue();
-            for ($i = 0;$i < 3 ;$i++) {
-              $option = array_values($options[$i]);
-              array_push($answers, $option[0]);
+        foreach ($node->get('field_quiz') as $key => $para) {
+          $question = [];
+          $answers = [];
+          if ($para->entity->getType() == 'leo_quiz') {  
+            if ($key == $last_question) {
+              $quiz = $para->entity;   
+              $question = $quiz->field_question->value;
+              $answer = $quiz->field_answer->value;
+              array_push($answers, $answer);
+              $options = $quiz->field_option->getValue();
+              for ($i = 0;$i < 3 ;$i++) {
+                $option = array_values($options[$i]);
+                array_push($answers, $option[0]);
+              }
+              shuffle($answers);
+              $response_array['question'] = $question;
+              $response_array['answers'] = $answers;
             }
-            shuffle($answers);
-            array_push($response_array,[$question,$answers]);
+           
           }
         }
-
           $quiz_encoded = $this->serializer->serialize($response_array, 'json', ['plugin_id' => 'entity']);
           
           // Build response with node serialized
           $response = new JsonResponse([
-            'quiz' => $response_array,
+            'quiz' => $response_array[$last_question],
           //  'node' => $node_encoded,
           ]);
-      
-    //  return $response;     
       } else {
       
       }
